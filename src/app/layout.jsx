05f7105a0b8436f72dc0303928af5c6515100397
Den-Bratchikov <denbratch@gmail.com>
2023-01-17 16:56:58 +0500

    fix: remove app folder

--- src/app/layout.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react/prop-types
-const RootLayout = ({ children }) => (
-  <html lang="en">
-    <head />
-    <body>{children}</body>
-  </html>
-);
-
-export default RootLayout;
