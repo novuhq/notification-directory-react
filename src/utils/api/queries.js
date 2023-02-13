8b48d5595dda895c7e4f78ddbae5b397fde0944c
Nevo David <nevo@notu.co>
2023-02-14 01:53:45 +0200

    feat: notifications

--- src/utils/api/queries.js
+++ src/utils/api/queries.js
@@ -1,18 +1,17 @@
-let categories = undefined;
+let categories;
 const getCategories = async () => {
   try {
     if (categories) {
       return categories;
     }
-    const categoriesResponse = await fetch(`https://${process.env.REACT_APP_API}/categories/`);
+    const categoriesResponse = await fetch(`https://api.notifications.directory/categories/`);
     categories = await categoriesResponse.json();
     if (!Array.isArray(categories)) {
       categories = undefined;
       return [];
     }
     return categories;
-  }
-  catch(e) {
+  } catch (e) {
     return [];
   }
 };
@@ -21,15 +20,14 @@ const getSubCategories = async (categoryId) => {
   try {
     console.log(categoryId);
     const subCategoriesResponse = await fetch(
-        `https://${process.env.REACT_APP_API}/categories/${categoryId}/sub`
+      `https://api.notifications.directory/categories/${categoryId}/sub`
     );
     const subCategories = await subCategoriesResponse.json();
     if (!Array.isArray(subCategories)) {
       return [];
     }
     return subCategories;
-  }
-  catch (err) {
+  } catch (err) {
     return [];
   }
 };
@@ -38,15 +36,14 @@ const getNotifications = async (subCategoryId) => {
   try {
     console.log(subCategoryId);
     const notificationsResponse = await fetch(
-        `https://${process.env.REACT_APP_API}/sub/${subCategoryId}/notifications`
+      `https://api.notifications.directory/sub/${subCategoryId}/notifications`
     );
     const notifications = await notificationsResponse.json();
     if (!Array.isArray(notifications)) {
       return [];
     }
     return notifications;
-  }
-  catch (err) {
+  } catch (err) {
     return [];
   }
 };
