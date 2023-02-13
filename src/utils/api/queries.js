c6c768d710a2197c6a02de3fae04cdf368ffbe29
Nevo David <nevo@notu.co>
2023-02-14 01:15:45 +0200

    Feat: notifications

--- src/utils/api/queries.js
+++ src/utils/api/queries.js
@@ -1,23 +1,38 @@
 const getCategories = async () => {
   const categoriesResponse = await fetch(`https://${process.env.REACT_APP_API}/categories/`);
-  const categories = await categoriesResponse.json();
-  return categories;
+  try {
+    const categories = await categoriesResponse.json();
+    return categories;
+  }
+  catch(e) {
+    return [];
+  }
 };
 
 const getSubCategories = async (categoryId) => {
   const subCategoriesResponse = await fetch(
     `https://${process.env.REACT_APP_API}/categories/${categoryId}/sub`
   );
-  const subCategories = await subCategoriesResponse.json();
-  return subCategories;
+  try {
+    const subCategories = await subCategoriesResponse.json();
+    return subCategories;
+  }
+  catch (err) {
+    return [];
+  }
 };
 
 const getNotifications = async (subCategoryId) => {
   const notificationsResponse = await fetch(
     `https://${process.env.REACT_APP_API}/sub/${subCategoryId}/notifications`
   );
-  const notifications = await notificationsResponse.json();
-  return notifications;
+  try {
+    const notifications = await notificationsResponse.json();
+    return notifications;
+  }
+  catch (err) {
+    return [];
+  }
 };
 
 export { getCategories, getSubCategories, getNotifications };
